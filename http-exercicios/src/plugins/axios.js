8984a1883fa8c89362d39dc2db08d341ab4aa213
maicom goncalves <your_github_email>
2022-02-09 18:10:43 -0400

    aplicação de formulario com bootstrap

---
+++ http-exercicios/src/plugins/axios.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+//axios.defaults.baseURL ='https://curso-vue-d0289-default-rtdb.firebaseio.com/'
+
+Vue.use({
+  install(Vue){
+    //Vue.prototype.$http = axios
+    Vue.prototype.$http=axios.create({
+      baseURL :'https://curso-vue-d0289-default-rtdb.firebaseio.com/',
+      headers:{
+        "Authorization":"abc123"
+      }
+    })
+    Vue.prototype.$http.interceptors.request.use(config =>{
+      console.log(config.method)
+      /*if(config.method == 'post'){
+        config.method = 'put'
+      }*/
+      return config
+    },error => Promise.reject(error))
+
+    Vue.prototype.$http.interceptors.response.use(res =>{
+      /*const array =[]
+      for(let chave in res.data){
+        array.push({id:chave,...res.data[chave]})
+      }
+      res.data = array*/
+      return res
+    },error => Promise.reject(error))
+  }
+})
